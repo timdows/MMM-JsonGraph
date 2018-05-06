735117c15a9642109583f33865b4354137abbc7a
Timdows <tim@inexpro.nl>
2018-05-06 22:57:42 +0200

    Code cleanup

--- MMM-JsonGraph.js
+++ MMM-JsonGraph.js
@@ -75,41 +75,26 @@ Module.register("MMM-JsonGraph", {
 				"Maybe the config arrayName is not used and should be, or is configured wrong";
 		}
 
-		var b = "";
+		var rects = "";
+		var maxValue = Math.max.apply(Math, array.map(function (o) { return o[this.config.xAxisName]; }))
 		var indexHeight = 0;
+
 		items.forEach(element => {
-			b += `<g class="mmmJsonGraph-bar">
-				<rect width="40" height="19" y="` + 20 * indexHeight + `"></rect>
-				<text x="45" y="9.5" dy=".35em">` + element[this.config.xAxisName] + `</text>
+			var xAxisValue = element[this.config.xAxisName];
+			var width = (value / maxValue) * 100;
+			var height = 20 * indexHeight;
+			rects += `<g class="mmmJsonGraph-bar">
+				<rect width="` + width + `%" height="19" y="` + height + `"></rect>
+				<text x="` + width + `%" y="` + (height + 8) + `" dy=".35em">` + + ` steps</text>
 			</g>`;
 			indexHeight++;
 		});
 
-		var a = `<figure>
-	  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="chart" width="300" height="150" role="img">
-		` + b + `
-		<g class="mmmJsonGraph-bar">
-		  <rect width="40" height="19"></rect>
-		  <text x="45" y="9.5" dy=".35em">4 apples</text>
-		</g>
-		<g class="mmmJsonGraph-bar">
-		  <rect width="80" height="19" y="20"></rect>
-		  <text x="85" y="28" dy=".35em">8 bananas</text>
-		</g>
-		<g class="mmmJsonGraph-bar">
-		  <rect width="150" height="19" y="40"></rect>
-		  <text x="150" y="48" dy=".35em">15 kiwis</text>
-		</g>
-		<g class="mmmJsonGraph-bar">
-		  <rect width="160" height="19" y="60"></rect>
-		  <text x="161" y="68" dy=".35em">16 oranges</text>
-		</g>
-		<g class="mmmJsonGraph-bar">
-		  <rect width="230" height="19" y="80"></rect>
-		  <text x="235" y="88" dy=".35em">23 lemons</text>
-		</g>
-	  </svg>
-	  </figure>`;
-		return a;
+		return `
+		<figure>
+			<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="chart" width="300" height="150" role="img">
+				` + rects + `
+	  		</svg>
+	  	</figure>`;
 	}
 });
